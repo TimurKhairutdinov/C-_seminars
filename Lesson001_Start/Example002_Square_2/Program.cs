95666df8b2eda9a33521776b808dd8654a25a7d0
Timur <t1m.1h@yandex.ru>
2022-04-28 15:13:02 +1200

    add Example 002 Square 2

---
+++ Lesson001_Start/Example002_Square_2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 1. Программа, которая принимает 2 числа и проверяет, является ли первое квадратом второго.
+
+Console.Write("Введите первое число: ");
+int NumberOne = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int NumberTwo = Convert.ToInt32(Console.ReadLine());
+double square = NumberTwo / NumberOne;
+Console.WriteLine("Является ли первое число квадратом второго?");
+if (square == NumberOne)
+{
+    Console.Write("Да");
+}
+else
+{
+    Console.Write("Нет");
+}

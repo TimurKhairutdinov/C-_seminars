add4247568452906e490692271f1b07514fadc0d
Timur <t1m.1h@yandex.ru>
2022-04-28 15:46:46 +1200

    add Example 003 Week

---
+++ Lesson001_Start/Example003_Week/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 2. Программа, которая выдаёт название дня недели по заданному номеру.
+
+Console.Write("Введите число: ");
+int day = Convert.ToInt32(Console.ReadLine());
+
+if (day == 1)
+{
+    Console.Write("Понедельник");
+}
+if (day == 2)
+{
+    Console.Write("Вторник");
+}
+if (day == 3)
+{
+    Console.Write("Среда");
+}
+if (day == 4)
+{
+    Console.Write("Четверг");
+}
+if (day == 5)
+{
+    Console.Write("Пятница");
+}
+if (day == 6)
+{
+    Console.Write("Суббота");
+}
+if (day == 7)
+{
+    Console.Write("Воскресенье");
+}

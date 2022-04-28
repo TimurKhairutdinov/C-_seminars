7b595b93509251e6c3283ef9b054b58c172efed8
Timur <t1m.1h@yandex.ru>
2022-04-28 18:48:02 +1200

    add Example 004 Numbers N

---
+++ Lesson001_Start/Example004_Numbers_N/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача. Написать программу, которая на вход принимает одно число N, 
+// а на выходе показывает все целые числа в промежутке от -N до N.
+
+Console.Write("Введите число: ");
+int NumberOne = Convert.ToInt32(Console.ReadLine());
+int NumberTwo = (NumberOne * -1);
+
+while ( NumberTwo <= NumberOne )
+{
+    Console.Write(NumberTwo + ",");
+    NumberTwo++;
+}

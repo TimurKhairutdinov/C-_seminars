e45eda07138be347ff2a461cdcbedc7b31b851fc
Timur <t1m.1h@yandex.ru>
2022-05-03 15:01:55 +1200

    add Example 003 Random Number Max

---
+++ Lesson002Array&Function/Example001/Program.cs
@@ -0,0 +1,23 @@
+﻿//
+int Sum(int number1, int number2)
+{
+    int sum = number1+number2;
+    return sum;
+}
+
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+int sum = Sum(num1, num2);
+Console.WriteLine(sum);
+
+
+// Типы объявления массивов:
+int [] numbers1 = {1,3,4,5,6};
+int [] numbers2 = new int[5];
+int [] numbers3 = new[5];
+int [] numbers4; // Массив объявлен, но не проинициализирован.
+// Если в массив не заполен, то при создании он заполнится пустыми значениями, исходя из типа данных.
+
+int number = 0; // 0
+string str = ""; // Пустая строка.
+bool b1 = false; // false.
---
+++ Lesson002Array&Function/Example002/Program.cs
@@ -0,0 +1,29 @@
+﻿int[] numbers1 = new int [3]; // Создание массива без объявления типа не работает. Почему-то.
+int[] numbers2 = new int [5];
+
+void FillIntArray (int[] array) // Процедура заполнения массива. Без возвращаемого значения.
+
+{
+    int index = 0;
+    while (index < array.Length)
+    {
+        array[index] = new Random().Next(0,100);
+        Console.Write(array[index] + ", ");
+        index++;
+    }
+
+}
+
+int Sum (int a, int b) //Функция возвращает значение. В данном случае  return sum; 
+{
+    int sum = a + b;
+    return sum;
+}
+
+int sum = Sum(4,5); // Переменная для записи результата отработанной функции Sum.
+
+FillIntArray(numbers1);
+Console.WriteLine();
+FillIntArray(numbers2);     
+Console.WriteLine();
+Console.WriteLine(Sum(5,5));

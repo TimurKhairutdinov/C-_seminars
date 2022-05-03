b04394f253e2e5195ceb5bc56a17dfff7c6d319c
Timur <t1m.1h@yandex.ru>
2022-05-03 21:44:17 +1200

    add example 005 three number deleted second

---
+++ Lesson002Array&Function/Example005ThreeNumberDeletSecond/Program.cs
@@ -0,0 +1,41 @@
+﻿// Напишите программу, которая выводит случайное трёх значное число и удаляет вторую цифру этого числа.
+int number = RandomInt(100,1000);
+
+int RandomInt(int a, int b)
+{
+    return new Random().Next(a,b);
+}
+Console.WriteLine(number);
+int Total(int number)
+{
+    int a = number/100; // int a 516/100 = 5
+    int c = number % 10;// int c 516 % 10 = 6
+    int b = a*10+c;
+    return b;
+}
+
+int Div3(int a)
+{
+    int b = a % 10;
+    return b;
+}
+
+int Div2(int a)
+{ 
+    int b = a/10;
+    int c = b%10;
+    return c;
+}
+
+int Div1(int a)
+{
+    int b = a/100;
+    return b;
+}
+Console.Write(Div1(number));
+// Console.WriteLine(Div2(number));
+Console.WriteLine(Div3(number));
+
+Console.WriteLine(Total(number)); // Одна функция на всё решение.
+
+Console.WriteLine(Convert.ToString(Div1(number)) + Convert.ToString(Div3(number))); // Решение через "склеивание" переменных в string.

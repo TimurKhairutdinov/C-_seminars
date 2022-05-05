8af948b712e451df6313c2cf924d1cdfab6506f8
Timur <t1m.1h@yandex.ru>
2022-05-06 03:20:57 +1200

    add example 002 range x & y

--- Lesson003Array&Function2/Example002RangeX&Y/Program.cs
+++ Lesson003Array&Function2/Example002RangeX&Y/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая по заданному номеру четверти, 
+//  показывает диапазон возможных координат точек в этой четверти (x & y).
+Console.Write("Введите номер четверти: ");
+int quarter = Convert.ToInt32(Console.ReadLine());
+int min = int.MinValue;
+int max = int.MaxValue;
+if (quarter <= 0 || quarter > 4)
+{
+    Console.WriteLine("Error");
+    return;
+}
+if (quarter == 1)
+{
+    Console.WriteLine($"Диапазон для X: от {min} до 0");
+    Console.WriteLine($"Диапазон для Y: от 0 до {max}");
+}
+if (quarter == 2)
+{
+    Console.WriteLine($"Диапазон для X: от 0 до {max}");
+    Console.WriteLine($"Диапазон для Y: от 0 до {max}");
+}
+if (quarter == 3)
+{
+    Console.WriteLine($"Диапазон для X: от {min} до 0");
+    Console.WriteLine($"Диапазон для Y: от {min} до 0");
+}
+if (quarter == 4)
+{
+    Console.WriteLine($"Диапазон для X: от 0 {max}");
+    Console.WriteLine($"Диапазон для Y: от {min} до 0");
+}

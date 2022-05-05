5e5ffde33e8068898e74311b69f18da99d780df4
Timur <t1m.1h@yandex.ru>
2022-05-05 17:26:53 +0300

    add lesson 003 example 001 x & y

---
+++ Lesson003Array&Function2/Example001X&Y/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 17. Напишите программу, которая принимает на вход координаты точки (x,y), причём x != 0, y != 0,
+// и выдаёт номер четверти плоскости, в которой находится эта точка. 
+//     Y
+//  1  |  2
+// ____|____ X
+//     |
+//  3  |  4
+// 
+Console.Write("X: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+// Int.MaxValue Максимальное значение int.
+// Int.MinValue Минимальное значение int.
+if (x == 0 || y == 0) // Две палочки || - оператор "или".
+{
+    Console.WriteLine("error");
+    return;
+}
+
+int quarterNumber = 0;
+
+if (x < 0 & y > 0)  // & или && - оператор "и". 
+{
+    quarterNumber = 1;
+}
+
+if (x > 0 & y > 0)
+{
+    quarterNumber = 2;
+}
+
+if (x < 0 & y < 0)
+{
+    quarterNumber = 3;
+}
+
+if (x > 0 & y < 0)
+{
+    quarterNumber = 4;
+}
+
+Console.Write($"Точка находится в чеверти номер: {quarterNumber}");

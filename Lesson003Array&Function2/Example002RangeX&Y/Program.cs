2f03651cf0e0096a0d8edffe85d0f17cfa95e2ad
Timur <t1m.1h@yandex.ru>
2022-05-06 10:58:06 +0300

    add example 003 find distance x & y

--- Lesson003Array&Function2/Example002RangeX&Y/Program.cs
+++ Lesson003Array&Function2/Example002RangeX&Y/Program.cs
@@ -1,6 +1,6 @@
 ﻿// Напишите программу, которая по заданному номеру четверти, 
 //  показывает диапазон возможных координат точек в этой четверти (x & y).
-Console.Write("Введите номер четверти: ");
+Console.Write("Введите номер четверти от 1 до 4: ");
 int quarter = Convert.ToInt32(Console.ReadLine());
 int min = int.MinValue;
 int max = int.MaxValue;
---
+++ Lesson003Array&Function2/Example003FindDistanceX&Y2D/Program.cs
@@ -0,0 +1,39 @@
+﻿/* Задача 21. Напишите программу, которая принимает на вход координаты двух точек
+и находит расстояние между ними в 2D пространстве 
+
+*/
+
+Console.Write("Введите X первой точки: ");
+int xA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите Y первой точки: ");
+int yA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите X второй точки: ");
+int xB = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите Y второй точки: ");
+int yB = Convert.ToInt32(Console.ReadLine());
+
+
+int xC = xA;
+int yC = yB;
+
+Console.WriteLine($"A({xA},{yA})");
+Console.WriteLine($"B({xB},{yB})");
+Console.WriteLine($"C({xC},{yC})");
+
+double Gip(int yA, int yC, int xC, int xB)
+{
+    int ac;
+    int bc;
+    int ab2;
+    // if (yA > yC) ac = yA - yC;
+    ac = yC - yA; 
+   // if (xC > xB) bc = xC - xB;
+    bc = xB - xC;
+    ab2 = (ac * ac) + (bc * bc);
+    return Math.Sqrt(ab2);
+    // Это же решение:
+    // return Math.Sqrt(Math.Pow(ac,2)+ Math.Pow(bc,2));  
+}
+
+Console.WriteLine("Расстояние между заданными точками A и B: " + Gip(yA, yC, xC, xB));

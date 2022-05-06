1d245dae0e1910ba6818c2235ea261167843fc22
Timur <t1m.1h@yandex.ru>
2022-05-06 12:12:44 +0300

    add Example 004 Quatro Table

---
+++ Lesson003Array&Function2/Example004QuatroTable/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход число N и выдаёт таблицу квадратов чисел от 1 до N.
+Console.Write("N: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write("Степень преобразования: ");
+int stepen = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+while (count <= number)
+{
+    int countQuatro = Convert.ToInt32(Math.Pow(count,stepen));
+    Console.WriteLine($"{count}^{stepen} = {countQuatro}");
+    count++;
+}

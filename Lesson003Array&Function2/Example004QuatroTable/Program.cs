a453f40c52abf694622768c4e3506bc3d90b6588
Timur <t1m.1h@yandex.ru>
2022-05-06 13:00:59 +0300

    correct Example 004

--- Lesson003Array&Function2/Example004QuatroTable/Program.cs
+++ Lesson003Array&Function2/Example004QuatroTable/Program.cs
@@ -2,7 +2,7 @@
 Console.Write("N: ");
 int number = Convert.ToInt32(Console.ReadLine());
 Console.Write("Степень преобразования: ");
-int stepen = Convert.ToInt32(Console.ReadLine());
+int stepen = Convert.ToInt32(Console.ReadLine()); // Позволяет ввести любую степень, а не только 2 как указано в условии.
 int count = 1;
 while (count <= number)
 {

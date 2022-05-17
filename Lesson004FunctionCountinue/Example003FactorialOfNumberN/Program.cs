5a55e9f5c9c2618a4f7a32be6ad0097d5e8a988a
Timur <t1m.1h@yandex.ru>
2022-05-17 19:57:50 +0300

    add lesson 004 function countinue example 003 factorial of number n

---
+++ Lesson004FunctionCountinue/Example003FactorialOfNumberN/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 28. Напишите программу, которая принимает на вход число N выдаёт произведение чисел от 1 до N.
+
+int number = Convert.ToInt32(Console.ReadLine());
+int result = 1;
+for (int i = 1; i <= number; i++)
+{
+    result *= i;
+    //result = result * i;
+}
+Console.WriteLine(result);

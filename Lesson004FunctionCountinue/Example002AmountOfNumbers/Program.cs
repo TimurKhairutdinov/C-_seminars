5a723e911849a1b8fc0df7ddc43adff60e8f46a6
Timur <t1m.1h@yandex.ru>
2022-05-17 19:12:42 +0300

    add lesson 004 example 002

---
+++ Lesson004FunctionCountinue/Example002AmountOfNumbers/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 26. Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+
+// Ввод числа через консоль. 
+string numbers = Console.ReadLine();
+// Переменная размер массива.
+int size = numbers.Length;
+// Вывод размера в консоль.
+Console.WriteLine(size);
+
+// Решение через переменную int
+int numbers2 = Convert.ToInt32(Console.ReadLine());
+int qty = 0; // qty == qountity 
+while (numbers2 != 0)
+{
+    numbers2 = numbers2/10;
+    qty++;
+}
+Console.WriteLine(qty);

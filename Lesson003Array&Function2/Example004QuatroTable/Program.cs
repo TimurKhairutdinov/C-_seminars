c6e4187ff3fc62480e346b4763ba81a6ec885d48
Timur <t1m.1h@yandex.ru>
2022-05-17 17:51:18 +0300

    add lesson 004 function continue example 001 sum of numbers from one to a

--- Lesson003Array&Function2/Example004QuatroTable/Program.cs
+++ Lesson003Array&Function2/Example004QuatroTable/Program.cs
@@ -4,9 +4,10 @@ int number = Convert.ToInt32(Console.ReadLine());
 Console.Write("Степень преобразования: ");
 int stepen = Convert.ToInt32(Console.ReadLine()); // Позволяет ввести любую степень, а не только 2 как указано в условии.
 int count = 1;
+if (number < 0) number = -number;
 while (count <= number)
 {
-    int countQuatro = Convert.ToInt32(Math.Pow(count,stepen));
+    int countQuatro = Convert.ToInt32(Math.Pow(count, stepen));
     Console.WriteLine($"{count}^{stepen} = {countQuatro}");
     count++;
 }
---
+++ Lesson004FunctionCountinue/Example001SumOfNumbersFromOneToA/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 24. Напишите программу, которая принимает на вход число A и выдаёт сумму чисел от 1 до А.
+Console.Write("Введите число: ");
+int numbers = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+for (int i = 1; i <= numbers; i++)
+{
+    sum += i;
+    //sum = sum + i;
+}
+Console.WriteLine("Ответ: " + sum);

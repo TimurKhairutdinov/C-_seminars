865cc5d0cc2e397e6a48af6b53ce73d62c2249f8
Timur <t1m.1h@yandex.ru>
2022-05-23 19:21:46 +0300

    add example 003 reverse

---
+++ Lesson005FunctionContinue2/Example003Reverse/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 32: Напишите программу замена элементов массива:
+// положительные элементы замените на соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2] 
+
+int[] numbers = new int[5];
+
+// FillArray(numbers);
+RandomFillArray(numbers,-100,101);
+Reverse(numbers);
+void FillArray(int[] array)
+{
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write($"Введите число {i + 1}: ");
+        numbers[i] = Convert.ToInt32(Console.ReadLine());
+    }
+
+}
+void Reverse(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        numbers[i] = -numbers[i];
+        Console.Write($"{numbers[i]} ");
+    }
+}
+void RandomFillArray(int[]array, int start, int end)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        numbers[i] = new Random().Next(start,end);
+        Console.Write($"{numbers[i]} ");
+    }
+    Console.WriteLine();
+}
+
+
+
+
+
+
+
+
+
+
+
+

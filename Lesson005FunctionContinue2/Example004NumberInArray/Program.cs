ebcf7696e9c403726c089600eb2aa2ec9b2745b6
Timur <t1m.1h@yandex.ru>
2022-05-23 19:22:33 +0300

    add example 004 find number in array

---
+++ Lesson005FunctionContinue2/Example004NumberInArray/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// -3; массив [6, 7, 19, 345, 3] -> да
+
+
+int[] numbers = new int[5];
+FillArray(numbers);
+
+Console.Write("Введите искомое число: ");
+int findNumber = Convert.ToInt32(Console.ReadLine());
+if(findNumber < 0)
+{
+    findNumber = -findNumber;
+}
+FindNumber(numbers, findNumber);
+
+void FillArray(int[] numbers)
+{
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write($"Введите число {i + 1}: ");
+        numbers[i] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+void FindNumber(int[] numbers, int findNumber)      
+{
+    for (int i = 0; i < numbers.Length; i++)
+    {   
+        if(numbers[i] < 0)
+        {
+            numbers[i] = -numbers[i];
+        }
+        if (numbers[i] == findNumber)
+        {
+            Console.WriteLine($"Число {findNumber} найдено, индекс: {i}");
+            break; 
+        }
+        if (i + 1 == numbers.Length)
+        {
+            Console.WriteLine("Число отсутствует!");
+        }
+    }
+}

9100b25d1edbcf6b73823177ac69db09afe4aced
Timur.Khairutdinov <t1m.1h@yandex.ru>
2022-05-28 16:13:46 +0300

    add example 005 Copy Array

---
+++ Lesson006TwoDemensionalArray&Recursion/Example005CopyArray/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 45: Напишите программу, которая будет создавать копию заданного массива 
+// с помощью поэлементного копирования.
+
+int[] array = new int[5];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0,10);
+    Console.Write(array[i]);
+}
+Console.WriteLine();
+string copyArray = String.Empty;
+for (int i = 0; i < array.Length; i++)
+{
+    copyArray += array[i];
+}
+Console.WriteLine(copyArray);

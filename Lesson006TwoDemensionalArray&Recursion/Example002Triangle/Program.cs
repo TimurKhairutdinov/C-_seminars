e405425c385cd4ece2a6c4a10e8ffd08323e9c4e
Timur.Khairutdinov <t1m.1h@yandex.ru>
2022-05-28 16:10:52 +0300

    add Example 002 Triangle

---
+++ Lesson006TwoDemensionalArray&Recursion/Example002Triangle/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 40: Напишите программу, которая принимает на вход три числа и проверяет, 
+// может ли существовать треугольник с сторонами такой длины.
+
+int ab = Input("AB");
+int ac = Input("AC");
+int bc = Input("BC");
+if (ab < ac + bc && bc < ab + ac && ac < ab + bc)
+{
+    Console.WriteLine("Треугольник может существовать!");
+}
+else 
+{
+    Console.WriteLine("Треугольник не может существовать!");
+}
+
+int Input(string name)
+{
+    Console.Write($"Введите сторону {name}: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}

8ee180e5fdd1b45587b7332e7f5648801a897075
Timur.Khairutdinov <t1m.1h@yandex.ru>
2022-06-15 16:10:57 +0300

    add example 002 Two Dimensional Array & Fill Random

---
+++ Lesson007TwoDimensionalArray/Example002TwoDemArrFillRandom/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задайте двумерный массив размером m x n, заполненный случайными числами.
+Console.WriteLine("Введите количество строк: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] arr = new int[m, n];
+
+RandomFillArray(arr, -100, 101);
+PrintArray(arr);
+
+void RandomFillArray(int[,] array, int min, int max)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().Next(min, max);
+        }
+    }
+}
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}

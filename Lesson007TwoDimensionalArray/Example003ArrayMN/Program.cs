90e4bbc4d7fba615f897829e21edb53b4f4f132f
Timur.Khairutdinov <t1m.1h@yandex.ru>
2022-06-15 16:12:12 +0300

    add example 003 Array Amn = m+n

---
+++ Lesson007TwoDimensionalArray/Example003ArrayMN/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 48. Задайте двумерный массив размера m на n, 
+// каждый элемент массива находится по формуле Amn = m+n.
+// Выведите полученный массив на экран.
+Console.Write("Введите количество строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int[,] arr = new int[rows, columns];
+
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        arr[i, j] = i + j;
+    }
+}
+
+PrintArray(arr);
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}

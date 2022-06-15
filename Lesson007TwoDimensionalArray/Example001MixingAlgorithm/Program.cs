c5d5a78757f2f443126c50d122d3f5983365c131
Timur.Khairutdinov <t1m.1h@yandex.ru>
2022-06-15 15:30:30 +0300

    add lesson 007 Two Dimensional Array Example 001 Mixing Algorithm

---
+++ Lesson007TwoDimensionalArray/Example001MixingAlgorithm/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+for (int i = 0; i < arr.Length; i++)
+{
+    int randomIndex = new Random().Next(0, arr.Length);
+    int temp = arr[i];
+    arr[i] = arr[randomIndex];
+    arr[randomIndex] = temp;
+}
+
+Console.WriteLine();
+
+for (int i = 0; i < arr.Length; i++)
+{
+    Console.Write(arr[i] + " ");
+}
